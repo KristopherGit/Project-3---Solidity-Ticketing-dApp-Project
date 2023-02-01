4690264df73a25b31cfdc510012d9374900f5b74
Christopher Ringwood <chris@Christophers-MacBook-Air.local>
2023-02-01 00:09:06 -0500

    Updated ticketholder.sol file

--- ticketholder.sol
+++ ticketholder.sol
@@ -4,11 +4,14 @@ pragma solidity ^0.5.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
 
 contract TickETHolder is ERC721 {
-        address public contractOwner;
+        address payable contractOwner;
         // Instantiate instance of the maximum total number of concert tickets to be minted (public viewable call variable)
         uint256 public MAX_TICKETS;
         // Instantiate instance of the ticketCount variable
         uint256 public ticketCount;
+        // Instantiate _seatsMintedSoFar as a global variable to keep track of while minting
+        // Set initially to 0, once contract is deployed it will update
+        uint public _seatsMintedSoFar = 0; 
     // Structure of the TickETHholder TicketData obj 
     struct TicketData {
         // Owner Info
@@ -46,35 +49,77 @@ contract TickETHolder is ERC721 {
 
     // Create mint function to produce all tickets for the event at once (modify if advance ticket sale required)
     function mint(
-        address _owner,
-        string memory _ownerFirstName,
-        string memory _ownerLastName,
-        string memory _eventName,
-        uint _concertDate,
-        uint _price, 
-        string memory _venueName,
-        uint _seatNumber,
-        string memory _seatColor
-    ) public {
-    // Make requirement that the number of tickets minted is not over the MAX_TICKETS value indicated above
-    require(ticketCount < MAX_TICKETS, "Max tickets for venue has been reached."); 
-    // Increment total ticket count as mint function iterates through all tickets being minted
-    ticketCount++;
+            //address payable contractOwner,
+            string memory _ownerFirstName,
+            string memory _ownerLastName,
+            string memory _eventName,
+            uint _concertDate,
+            uint _price, 
+            string memory _venueName,
+            //uint _seatNumber,
+            string memory _seatColor, 
+            uint batchSize
+        ) public {
+        require(msg.sender == contractOwner, "Only the contract owner[deployer] can mint tickets.");
 
-    // Mint ticket with specific metadata
-    ticketData[ticketCount].owner = _owner;
-    ticketData[ticketCount].ownerFirstName = _ownerFirstName;
-    ticketData[ticketCount].ownerLastName = _ownerLastName;
-    ticketData[ticketCount].eventName = _eventName;
-    ticketData[ticketCount].concertDate = _concertDate; 
-    ticketData[ticketCount].price = _price;
-    ticketData[ticketCount].venueName = _venueName;
-    ticketData[ticketCount].seatNumber = _seatNumber; 
-    ticketData[ticketCount].seatColor = _seatColor;
-    }
+        uint remaining = MAX_TICKETS - _seatsMintedSoFar;
+        uint numToMint = remaining > batchSize ? batchSize : remaining;
+        for (uint i = _seatsMintedSoFar; i < _seatsMintedSoFar + numToMint; i++) {
+                // Mint a ticket with seat number `i`
 
-    function setMAX_TICKETS (uint _maxNumberOfTickets) private {
+                // Make requirement that the number of tickets minted is not over the MAX_TICKETS value indicated above
+                require(ticketCount < MAX_TICKETS, "Max tickets for venue has been reached."); 
+                // Increment total ticket count as mint function iterates through all tickets being minted
+                ticketCount++;
+
+                // Mint ticket with specific metadata
+                ticketData[ticketCount].owner = contractOwner;
+                ticketData[ticketCount].ownerFirstName = _ownerFirstName;
+                ticketData[ticketCount].ownerLastName = _ownerLastName;
+                ticketData[ticketCount].eventName = _eventName;
+                ticketData[ticketCount].concertDate = _concertDate; 
+                ticketData[ticketCount].price = _price;
+                ticketData[ticketCount].venueName = _venueName;
+                ticketData[ticketCount].seatNumber = _seatsMintedSoFar; 
+                ticketData[ticketCount].seatColor = _seatColor;
+            }
+        }
+
+    function setMAX_TICKETS (uint _maxNumberOfTickets) public {
         require(msg.sender == contractOwner);
         MAX_TICKETS = _maxNumberOfTickets;
+    }
+
+    function getSeatsMintedSoFar() public view returns (uint) {
+        return _seatsMintedSoFar;
+    }
+
+    //function getAllTicketData() public view returns (TicketData[] memory) {
+    //TicketData[] memory result = new TicketData[](ticketCount);
+    //uint j = 0;
+    //for (uint i = 1; i <= ticketCount; i++) {
+    //    result[j] = ticketData[i];
+    //    j++;
+    //}
+    //return result;
+    //}
+
+    event TicketPurchased(uint ticketId, address buyer, uint price);
+
+    function buyTicket(uint ticketId) public payable {
+        // Make sure to purchase the ticket the ticket is available (i.e. set to the contractOwner, so it's still in unsold stasis/pool of minted tickets
+        require(ticketData[ticketId].owner == contractOwner, "Ticket isn't available for sale. Already sold. Sorry. Try to purchase a different seat, please.");
+
+        // Make sure the buyer has enough money to buy the ticket
+        require(msg.value >= ticketData[ticketId].price, "Insufficent funds.");
+
+        // Send required funds from buyer -> contractOwner (owner)
+        contractOwner.transfer(msg.value);
+
+        // Reset the status of the ticketData attribute for the buyer's address to be the new owner ('address owner')
+        ticketData[ticketId].owner = msg.sender;
+
+        // Broadcast sale has been made & transfer of ticket to msg.sender (buyer) has been completed
+        emit TicketPurchased(ticketId, msg.sender, ticketData[ticketId].price);
     }
 }
